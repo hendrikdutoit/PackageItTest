89281c37e2548643a21defec85a053fd96d78fe3
Ann Other <ann.other@testmodule.com>
2022-01-06 19:25:53 +0200

    PackageIt initial project creation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 import sys
 
 sys.path.insert(
-    0, 'C:\\Users\\hendr\\AppData\\Local\\Temp\\packageit_qkbvptxl\\PackageItTest\\src'
+    0, 'C:\\Users\\hendr\\AppData\\Local\\Temp\\packageit_kajd1xx6\\PackageItTest\\src'
 )
 project = 'PackageItTest'
 copyright = '2022, Ann Other'
